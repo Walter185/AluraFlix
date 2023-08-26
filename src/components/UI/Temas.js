d6cdd84ac64f79bbcbb75a7d4dc7bd98a1b3143d
Walter Liendo <walter.liendo@gmail.com>
2023-08-25 22:53:12 -0300

    creacion del banner

--- src/components/UI/Temas.js
+++ src/components/UI/Temas.js
@@ -1,1 +1,6 @@
-export const oscuro = '#191919'
+export const oscuro = '#191919';
+export const semioscuro = "#5C5C5C";
+export const texto = "#FFFFFF";
+export const h1 = "1.5rem"; 
+export const primero = "#2A7AE4";
+export const oscuroligero = "#C2C2C2";
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,30 @@
 import { useContext } from "react";
 import { styled } from "styled-components";
 import { Contexto } from "../Contexto";
+import { Banner } from "../components/Banner";
 
 const Principal = styled.main`
     background: ${({ theme }) => theme.oscuro};
 `;
-
 
 export function Home() {
     const datos = useContext(Contexto);
     const { categorias, videos } = datos
     return (
         <Principal>
-
+            <Banner />
+            {
+                videos.length > 0 &&
+                categorias.map((categoria, indice) => {
+                    const cantidad_videos = videos.filter((dato) => dato.categoria + '' === '' + categoria.id );
+                    if(cantidad_videos.length > 0) {
+                        return (
+                            <Categoria categoria={categoria} key={indice} />
+                        );
+                    }
+                    return '';
+                })
+            }
         </Principal>
     )
 }
